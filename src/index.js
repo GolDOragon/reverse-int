bfad8dda90c919ecf71da9e4b761b95aeac50ee8
GolDOragon <56398970+GolDOragon@users.noreply.github.com>
2020-03-04 16:05:56 +0300

    feat: solve "Reverse Int" task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    str = String(n);
+    let reverse = "";
+    let str_end = 0;
+    if (n < 0) str_end = 1;
+
+    for (let i = str.length - 1; i >= str_end; i--) {
+        reverse += str[i];
+    }
+    return Number(reverse);
+};
